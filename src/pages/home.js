8b438fae49624cf94617171b434f4be2412dc028
cogsincogs <jamiewthomas13@gmail.com>
2023-07-20 14:53:50 +0100

    Give homepage access to user data

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function Home() {
+    const [user, setUser] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+
+        setLoading(true)
+
+        async function getUser() {
+            // get user and check if logged in. return user && user.loggedIn
+            const response = await fetch('http://localhost:8080/user_data', {
+                method: "GET",
+                mode: "cors",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            let result = await response.json()
+            setLoading(false)
+            setUser(result.user)
+        }
+        getUser()
+    }, [])
+
+    if (loading) return <span>Loading</span>
+
+    const username = user.username;
+
     return (
-        <NavLink className="home" to="http://localhost:8080/logout">Logout</NavLink>
+        <>
+            <p>Hello {username}</p>
+            <NavLink className="home" to="http://localhost:8080/logout">Logout</NavLink>
+        </>
+        
     )
 }
