360ceae01fa21a3e392e84adf0e1993302ad3d79
cogsincogs <jamiewthomas13@gmail.com>
2023-07-27 15:07:11 +0100

    Adjust flexbox spacing and add external link

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,27 @@
 import { NavLink } from 'react-router-dom'
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { Flex, Heading, Link, Text } from '@chakra-ui/react'
+import GoogleButton from '../components/googlebutton'
+import BorderBox from '../layouts/borderbox'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 export default function Login() {
     return (
-        <Flex bg='white' align='center' justify='center' w="100%" h="100vh">
-            <Flex border='3px' borderStyle='solid' borderColor='gray.400' borderRadius={25} align='center' justify='center' direction='column' m={5} w="70%" h="70vh" bg='gray.200'>
-                <Heading>Hello!</Heading>
-                <Text>
+        <BorderBox>
+            <Heading m={5}>Hello!</Heading>
+            <Flex align='center' justify='space-between' direction='column' w="90%">
+                <Text m={3}>
                     This is a demonstration of a login page. It uses an API coded in Express.js, as well 
                     as MongoDB, OAuth2.0, NodeJS, and React. The project also makes use of docker containers. 
-                    Code for this and other ongoing projects are available on my GitHub page.
+                    Code for this and other ongoing projects are available on my <Link color="blue" href="https://github.com/cogsincogs" isExternal>GitHub <ExternalLinkIcon/></Link> page.
                 </Text>
-                <span></span>
-                <Text>
+                <Text m={3}>
                     Once you have logged in using your Google account, you will be able to see when you last 
                     logged in (if you have logged in before), the number of times you've logged in, and you 
                     can leave yourself some notes which will be available to you on your next login. This is 
                     to demonstrate user authentication and the REST API.
                 </Text>
-                <Text><NavLink className="login" to="http://localhost:8080/auth/google">Login with Google</NavLink></Text>
             </Flex>
-        </Flex>
+            <NavLink className="login" to="http://localhost:8080/auth/google" m={5}><GoogleButton/></NavLink>
+        </BorderBox>  
     )
 }
