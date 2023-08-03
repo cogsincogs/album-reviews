00639e5e4223da854e8c8c59524e75d93027d8eb
cogsincogs <jamiewthomas13@gmail.com>
2023-08-03 14:46:17 +0100

    Remove unnecessary CSS module folder

--- src/components/googlebutton.js
+++ src/components/googlebutton.js
@@ -2,12 +2,11 @@
 
 import { FcGoogle } from 'react-icons/fc'
 import { Button, Center, Text } from '@chakra-ui/react'
-import styles from './ui/googlebutton.module.css'
 
 export default function GoogleButton() {
   return (
     <Center p={8}>
-      <Button className={styles.button} border='1px solid' borderColor='gray.300' w={'full'} maxW={'md'} variant={'outline'} leftIcon={<FcGoogle />}>
+      <Button backgroundColor='white' border='1px solid' borderColor='gray.300' w={'full'} maxW={'md'} variant={'outline'} leftIcon={<FcGoogle />}>
         <Center>
           <Text>Sign in with Google</Text>
         </Center>
--- src/components/notepad.js
+++ src/components/notepad.js
@@ -3,7 +3,7 @@ import { Text, Textarea } from "@chakra-ui/react";
 export default function Notepad() {
     return (
         <>
-            <Textarea placeholder='Make a note here' size='sm' resize='none' />
+            <Textarea placeholder='Make a note here' size='sm' resize='none' backgroundColor='white' border='1px solid' borderColor='gray.300' />
         </>
     )
 }
