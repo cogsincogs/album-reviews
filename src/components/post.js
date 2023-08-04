24c223ee92caeab086bfcc6c3960cdf59c322b6d
cogsincogs <jamiewthomas13@gmail.com>
2023-08-04 15:40:36 +0100

    Pass props through and encapsulate post in flexbox

--- src/components/post.js
+++ src/components/post.js
@@ -1,15 +1,15 @@
-import { Text, Divider } from "@chakra-ui/react";
+import { Text, Divider, Flex } from "@chakra-ui/react";
 import PostDate from "./postdate";
 import PostControls from "./postcontrols";
 
-export default function Post({date, content}) {
+export default function Post({index, date, content, deleteHandler}) {
 
     return (
-        <>
+        <Flex direction='column' w='100%'>
             <Divider borderColor='gray.500' mt='5px' mb='5px'/>
             <PostDate date={date} />
             <Text fontSize='14'>{content}</Text>
-            <PostControls />
-        </>
+            <PostControls index={index} deleteHandler={deleteHandler} />
+        </Flex>
     )
 }
