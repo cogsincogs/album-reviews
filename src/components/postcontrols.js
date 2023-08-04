208ab33e90ee59c02688430d92555a009bad30e1
cogsincogs <jamiewthomas13@gmail.com>
2023-08-04 15:40:09 +0100

    Link up delete button

--- src/components/postcontrols.js
+++ src/components/postcontrols.js
@@ -1,11 +1,11 @@
 import { Flex, Link } from "@chakra-ui/react";
 
-export default function PostControls() {
+export default function PostControls({index, deleteHandler}) {
     return (
         <>
             <Flex>
                 <Link fontSize='11' m={0.5} ml={0}>Edit</Link>
-                <Link fontSize='11' m={0.5}>Delete</Link>
+                <Link fontSize='11' m={0.5} onClick={(e) => deleteHandler(e, index)}>Delete</Link>
             </Flex>
         </>
     )
