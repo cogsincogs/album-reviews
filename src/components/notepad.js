3fe52123bae87309a9223a10184ba6dc4b8cb48c
cogsincogs <jamiewthomas13@gmail.com>
2023-08-04 15:57:47 +0100

    Add functionality to change text in posts

--- src/components/notepad.js
+++ src/components/notepad.js
@@ -62,6 +62,25 @@ export default function Notepad({user}) {
         getPostsArray()
     }
 
+    const handleEditPost = async (e, postIndex, postContent) => {
+        e.preventDefault()
+
+        await fetch('http://localhost:8080/posts/' + id, {
+            method: "PATCH",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                postIndex: postIndex,
+                content: postContent
+            })
+        }).catch(e => console.log(e))
+
+        getPostsArray()
+    }
+
     return (
         <>
             <Flex direction='column' align='center' justify='center' w='100%'>
@@ -69,7 +88,7 @@ export default function Notepad({user}) {
                 <Flex direction='column-reverse' align='flex-start' justify='flex-start' w='100%' maxH='40vh' overflowY='auto'>
                     {
                         postsArray.map((post, index) => {
-                            return <Post key={index} index={index} date={post.date} content={post.content} deleteHandler={handleDeletePost} />
+                            return <Post key={index} index={index} date={post.date} content={post.content} deleteHandler={handleDeletePost} editHandler={handleEditPost} />
                         })
                     }
                 </Flex>
--- src/components/postcontrols.js
+++ src/components/postcontrols.js
@@ -1,10 +1,16 @@
 import { Flex, Link } from "@chakra-ui/react";
 
-export default function PostControls({index, deleteHandler}) {
+export default function PostControls({index, deleteHandler, editHandler}) {
+
+    const editPost = async (e, index) => {
+        const content = "test" // This is temporary, open a modal and ask user for input
+        editHandler(e, index, content)
+    }
+
     return (
         <>
             <Flex>
-                <Link fontSize='11' m={0.5} ml={0}>Edit</Link>
+                <Link fontSize='11' m={0.5} ml={0} onClick={(e) => editPost(e, index)}>Edit</Link>
                 <Link fontSize='11' m={0.5} onClick={(e) => deleteHandler(e, index)}>Delete</Link>
             </Flex>
         </>
