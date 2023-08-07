7695ad0ec95f2f286ae7c9936685f01f1172acea
cogsincogs <jamiewthomas13@gmail.com>
2023-08-07 14:57:02 +0100

    Add modal for editing post

---
+++ src/components/editmodal.js
@@ -0,0 +1,47 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+export default function EditModal({index, userId, isOpen, onClose, editHandler}) {
+    const [content, setContent] = useState("")
+
+    useEffect(() => {
+        getContent()
+    }, [])
+
+    async function getContent() {
+        const response = await fetch(`http://localhost:8080/posts/${userId}?postIndex=${index}`, {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        let post = await response.json()
+        let result = await post.content
+        setContent(result)
+    }
+
+    const save = async (e, index, content) => {
+        e.preventDefault()
+        editHandler(e, index, content)
+        onClose(e)
+    }
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Edit note</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Textarea value={content} onChange={(e) => setContent(e.target.value)} />
+                </ModalBody>
+                <ModalFooter>
+                    <Button colorScheme='blue' mr={3} onClick={(e) => save(e, index, content)}>Save</Button>
+                    <Button variant='ghost' onClick={onClose}>Close</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
--- src/components/postcontrols.js
+++ src/components/postcontrols.js
@@ -1,18 +1,17 @@
-import { Flex, Link } from "@chakra-ui/react";
+import { Flex, Link, useDisclosure } from "@chakra-ui/react";
+import EditModal from "./editmodal";
 
-export default function PostControls({index, deleteHandler, editHandler}) {
-
-    const editPost = async (e, index) => {
-        const content = "test" // This is temporary, open a modal and ask user for input
-        editHandler(e, index, content)
-    }
+export default function PostControls({index, userId, deleteHandler, editHandler}) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
         <>
             <Flex>
-                <Link fontSize='11' m={0.5} ml={0} onClick={(e) => editPost(e, index)}>Edit</Link>
+                <Link fontSize='11' m={0.5} ml={0} onClick={onOpen}>Edit</Link>
                 <Link fontSize='11' m={0.5} onClick={(e) => deleteHandler(e, index)}>Delete</Link>
             </Flex>
+
+            <EditModal index={index} userId={userId} isOpen={isOpen} onClose={onClose} editHandler={editHandler} />
         </>
     )
 }
