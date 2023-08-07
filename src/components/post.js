85e72f7f949f233f65027638a7113e4a9c1f5ad5
cogsincogs <jamiewthomas13@gmail.com>
2023-08-07 14:54:24 +0100

    Pass userId to PostControls for use in GET request

--- src/components/post.js
+++ src/components/post.js
@@ -2,14 +2,14 @@ import { Text, Divider, Flex } from "@chakra-ui/react";
 import PostDate from "./postdate";
 import PostControls from "./postcontrols";
 
-export default function Post({index, date, content, deleteHandler, editHandler}) {
+export default function Post({index, userId, date, content, deleteHandler, editHandler}) {
 
     return (
         <Flex direction='column' w='100%'>
             <Divider borderColor='gray.500' mt='5px' mb='5px'/>
             <PostDate date={date} />
             <Text fontSize='14'>{content}</Text>
-            <PostControls index={index} deleteHandler={deleteHandler} editHandler={editHandler} />
+            <PostControls index={index} userId={userId} deleteHandler={deleteHandler} editHandler={editHandler} />
         </Flex>
     )
 }
