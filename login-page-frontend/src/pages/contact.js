1c0f75695f994d3a585077af3610641e124b92ab
cogsincogs <jamiewthomas13@gmail.com>
2023-09-15 15:29:48 +0100

    Add colour to links

--- login-page-frontend/src/pages/contact.js
+++ login-page-frontend/src/pages/contact.js
@@ -5,12 +5,12 @@ export default function ContactPage() {
     return (
         <>
             <DefaultLayout>
-                <Flex direction="column" m={5}>
-                    <Heading m={5}>Contact</Heading>
-                    <Link href="#">My CV (to be added)</Link>
-                    <Link href="mailto:jamiewthomas13@gmail.com">Click here to send me an email</Link>
-                    <Link href="https://www.linkedin.com/in/jamie-thomas-88b318198/">My LinkedIn</Link>
-                    <Link href="https://github.com/cogsincogs">My GitHub</Link>
+                <Flex direction="column" m="5">
+                    <Heading m="5" textAlign="center">Contact</Heading>
+                    <Link href="#" color="blue.500">My CV (to be added)</Link>
+                    <Link href="mailto:jamiewthomas13@gmail.com" color="blue.500">Click here to send me an email</Link>
+                    <Link href="https://www.linkedin.com/in/jamie-thomas-88b318198/" color="blue.500">My LinkedIn</Link>
+                    <Link href="https://github.com/cogsincogs" color="blue.500">My GitHub</Link>
                 </Flex>
             </DefaultLayout>
         </>
