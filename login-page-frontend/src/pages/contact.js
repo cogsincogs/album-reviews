9c3bb7df9537ebd0f71e47c4e00fc1bd13c3752b
cogsincogs <jamiewthomas13@gmail.com>
2023-09-15 15:41:06 +0100

    Make external links open in new tab

--- login-page-frontend/src/pages/contact.js
+++ login-page-frontend/src/pages/contact.js
@@ -1,5 +1,6 @@
 import { Flex, Heading, Link } from "@chakra-ui/react"
 import DefaultLayout from "../layouts/DefaultLayout"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
 
 export default function ContactPage() {
     return (
@@ -7,10 +8,10 @@ export default function ContactPage() {
             <DefaultLayout>
                 <Flex direction="column" m="5">
                     <Heading m="5" textAlign="center">Contact</Heading>
-                    <Link href="#" color="blue.500">My CV (to be added)</Link>
+                    <Link href="#" color="blue.500" isExternal>My CV (to be added) <ExternalLinkIcon mb="1" /></Link>
                     <Link href="mailto:jamiewthomas13@gmail.com" color="blue.500">Click here to send me an email</Link>
-                    <Link href="https://www.linkedin.com/in/jamie-thomas-88b318198/" color="blue.500">My LinkedIn</Link>
-                    <Link href="https://github.com/cogsincogs" color="blue.500">My GitHub</Link>
+                    <Link href="https://www.linkedin.com/in/jamie-thomas-88b318198/" color="blue.500" isExternal>My LinkedIn <ExternalLinkIcon mb="1"/></Link>
+                    <Link href="https://github.com/cogsincogs" color="blue.500" isExternal>My GitHub <ExternalLinkIcon mb="1" /></Link>
                 </Flex>
             </DefaultLayout>
         </>
