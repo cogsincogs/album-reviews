45ec03143d45335db44a90e6b7728d2bfe673897
cogsincogs <jamiewthomas13@gmail.com>
2023-09-18 12:01:37 +0100

    Add spacing

--- login-page-frontend/src/pages/contact.js
+++ login-page-frontend/src/pages/contact.js
@@ -1,6 +1,7 @@
 import { Flex, Heading, Link } from "@chakra-ui/react"
 import DefaultLayout from "../layouts/DefaultLayout"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { FaRegFilePdf } from 'react-icons/fa6'
 
 export default function ContactPage() {
     return (
@@ -8,9 +9,13 @@ export default function ContactPage() {
             <DefaultLayout>
                 <Flex direction="column" m="5">
                     <Heading m="5" textAlign="center">Contact</Heading>
+                    <br/>
                     <Link href="#" color="blue.500" isExternal>My CV (to be added) <ExternalLinkIcon mb="1" /></Link>
+                    <br/>
                     <Link href="mailto:jamiewthomas13@gmail.com" color="blue.500">Click here to send me an email</Link>
+                    <br/>
                     <Link href="https://www.linkedin.com/in/jamie-thomas-88b318198/" color="blue.500" isExternal>My LinkedIn <ExternalLinkIcon mb="1"/></Link>
+                    <br/>
                     <Link href="https://github.com/cogsincogs" color="blue.500" isExternal>My GitHub <ExternalLinkIcon mb="1" /></Link>
                 </Flex>
             </DefaultLayout>
