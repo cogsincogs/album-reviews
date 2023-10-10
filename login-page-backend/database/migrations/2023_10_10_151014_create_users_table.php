25e7c2eae1562942effff47e64669a8584baf2d4
cogsincogs <jamiewthomas13@gmail.com>
2023-10-10 17:52:53 +0100

    Set up database migration for creating users table

--- login-page-backend/database/migrations/2023_10_10_151014_create_users_table.php
+++ login-page-backend/database/migrations/2023_10_10_151014_create_users_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
+        //
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('google_id');
+            $table->string('google_token');
+            $table->string('google_refresh_token');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
         });
     }
 
@@ -27,6 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        //
         Schema::dropIfExists('users');
     }
 };
