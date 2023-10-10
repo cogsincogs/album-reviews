4fbe1b9fb5f9c30efc927439ee5869266bc9c776
cogsincogs <jamiewthomas13@gmail.com>
2023-10-10 17:54:23 +0100

    Customise User model

--- login-page-backend/app/Models/User.php
+++ login-page-backend/app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'google_id',
+        'google_token',
+        'google_refresh_token',
     ];
 
     /**
@@ -29,8 +31,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
         'remember_token',
+        'google_id',
+        'google_token',
+        'google_refresh_token',
     ];
 
     /**
@@ -40,6 +44,5 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
