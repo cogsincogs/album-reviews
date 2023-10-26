a5d1f1b36acddb69df773b1f01d3de0ce917a948
cogsincogs <jamiewthomas13@gmail.com>
2023-10-26 12:30:22 +0100

    Create AuthController to clean up routes in api.php

---
+++ login-page-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Initialise Google login process
+     */
+    public function login() {
+        return Socialite::driver('google')
+            ->with(['access_type' => 'offline'])
+            ->redirect();
+    }
+
+    /**
+     * Google callback function
+     */
+    public function callback() {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::updateOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'username' => $googleUser->name,
+            'firstname' => $googleUser->user['given_name'],
+            'thumbnail' => $googleUser->user['picture'],
+            'email' => $googleUser->email,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        incrementLoginCount(Auth::user());
+        updateCurrentLoginDate(Auth::user());
+
+        return redirect('http://localhost:3000/projects/login-page/home');
+    }
+
+    /**
+     * If Google calls the /api/auth/failure endpoint
+     */
+    public function failure() {
+        return "Something went wrong";
+    }
+
+    /**
+     * When logging out, update lastLogin variable, invalidate session & redirect to login page
+     */
+    public function logout(Request $req) {
+        if ($req->user()) {
+            updateLastLogin($req->user());
+        }
+
+        Auth::logout();
+        $req->session()->invalidate();
+        $req->session()->regenerateToken();
+        return redirect('http://localhost:3000/projects/login-page');
+    }
+
+    /**
+     * Provide relevant user data to the frontend
+     */
+    public function userData(Request $req) {
+        if (!$req->user()) {
+            
+            // Not logged in
+            return json_encode("");
+
+        } else {
+
+            // Logged in
+            return json_encode([
+                "user" => $req->user(),
+            ]);
+
+        }
+    }
+
+    // Private functions
+
+    /**
+     * This increments the variable tracking how many times the user has logged in
+     */
+    private function incrementLoginCount(User $user) {
+        // Update given user with the user's loginCount plus 1
+        $user->update(['loginCount' => $user->loginCount + 1]);
+        $user->save();
+    }
+    
+    /**
+     * This sets the currentLoginDate to the current time & date when the user logs in
+     */
+    private function updateCurrentLoginDate(User $user) {
+        // Update given user's currentLoginDate with current date
+        $user->update(['currentLoginDate' => time()]);
+    }
+    
+    /**
+     * This sets the lastLogin variable to the currentLoginDate when the user logs out
+     */
+    private function updateLastLogin(User $user) {
+        // Update given user's lastLogin with its value for currentLoginDate
+        $user->update(['lastLogin' => $user->currentLoginDate]);
+        $user->save();
+    }
+}
--- login-page-backend/routes/api.php
+++ login-page-backend/routes/api.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
-use App\Models\User;
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,75 +16,13 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-// These endpoints must use web middleware as they redirect the page
 Route::group(['middleware' => ['web']], function () {
     
-    // The link clicked by the user when logging in. Initialises Google login process.
-    Route::get('/auth/google', function () {
-        return Socialite::driver('google')
-            ->with(['access_type' => 'offline'])
-            ->redirect();
-    });
-    
-    // The callback function used by Google. Provides user info to backend.
-    Route::get('/auth/google/callback', function () {
-        $googleUser = Socialite::driver('google')->user();
-
-        //return $googleUser->user['given_name'];
-
-        $user = User::updateOrCreate([
-            'google_id' => $googleUser->id,
-        ], [
-            'username' => $googleUser->name,
-            'firstname' => $googleUser->user['given_name'],
-            'thumbnail' => $googleUser->user['picture'],
-            'email' => $googleUser->email,
-            'google_token' => $googleUser->token,
-            'google_refresh_token' => $googleUser->refreshToken,
-        ]);
-
-        Auth::login($user);
-
-        incrementLoginCount(Auth::user());
-        updateCurrentLoginDate(Auth::user());
-
-        return redirect('http://localhost:3000/projects/login-page/home');
-    });
-
-    // Log a message in console if /api/auth/failure is called by Google
-    Route::get('/auth/failure', function () {
-        return "Something went wrong";
-    });
-
-    // This endpoint provides the relevant user data to the frontend
-    Route::get('/user_data', function (Request $req) {
-
-        if (!$req->user()) {
-            
-            // Not logged in
-            return json_encode("");
-
-        } else {
-
-            // Logged in
-            return json_encode([
-                "user" => $req->user(),
-            ]);
-
-        }
-    });
-
-    Route::get('/logout', function (Request $req) {
-
-        if ($req->user()) {
-            updateLastLogin($req->user());
-        }
-
-        Auth::logout();
-        $req->session()->invalidate();
-        $req->session()->regenerateToken();
-        return redirect('http://localhost:3000/projects/login-page');
-    });
+    Route::get('/auth/google', [AuthController::class, 'login']);
+    Route::get('/auth/google/callback', [AuthController::class, 'callback']);
+    Route::get('/auth/failure', [AuthController::class, 'failure']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/user_data', [AuthController::class, 'userData']);
 
     Route::get('/posts/{id}', [PostController::class, 'getAllPosts']);
     Route::post('/posts/{id}', [PostController::class, 'newPost']);
@@ -97,21 +33,4 @@ Route::group(['middleware' => ['web']], function () {
         return json_encode(['token' => csrf_token()]);
     });
 });
-
-function incrementLoginCount(User $user) {
-    // Update given user with the user's loginCount plus 1
-    $user->update(['loginCount' => $user->loginCount + 1]);
-    $user->save();
-}
-
-function updateLastLogin(User $user) {
-    // Update given user's lastLogin with its value for currentLoginDate
-    $user->update(['lastLogin' => $user->currentLoginDate]);
-    $user->save();
-}
-
-function updateCurrentLoginDate(User $user) {
-    // Update given user's currentLoginDate with current date
-    $user->update(['currentLoginDate' => time()]);
-}
 
